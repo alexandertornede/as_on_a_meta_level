c1491c10794ba90e29a67cad53217f547602c441
Alexander Tornede <alexander.tornede@upb.de>
2021-03-16 14:20:54 +0100

    Move ensemble code to subfolder

--- plots/generate_all_plots.py
+++
@@ -1,9 +0,0 @@
-from plots.voting_violin_plot import plot as voting_violin_plot
-from plots.stacking_plot import plot as stacking_plot
-from plots.boosting_comparion_ploy import plot as samme
-from plots.bagging_base_learner_comparison_plot import plot as bagging_plot
-
-voting_violin_plot()
-#stacking_plot()
-samme()
-bagging_plot()
